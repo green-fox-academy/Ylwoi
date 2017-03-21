db406fe14987dfd629b70e42a00e2a213a159f5e
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-21 15:55:45 +0100

    average-of-input.py added, done

---
+++ week02/day_2/average-of-input.py
@@ -0,0 +1,10 @@
+__author__ = 'ylwoi'
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter a number:"))
+num3 = int(input("Enter a number:"))
+num4 = int(input("Enter a number:"))
+num5 = int(input("Enter a number:"))
+sums = num1 + num2 + num3 + num4 + num5
+average = sums / 5
+print("Sum:", sums)
+print("Average:", average)
