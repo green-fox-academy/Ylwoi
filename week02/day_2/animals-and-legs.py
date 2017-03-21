dbee05c1520cbe79e713508d7c0481c11ac21c3f
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-21 15:48:46 +0100

    animals-and-legs.py added, done

---
+++ week02/day_2/animals-and-legs.py
@@ -0,0 +1,5 @@
+__author__ = 'ylwoi'
+chicken = int(input("Number of chickens?"))
+pig = int(input("Number of pigs?"))
+all_legs = chicken*2 + pig*4
+print("There are", all_legs, "legs in the farm.")
