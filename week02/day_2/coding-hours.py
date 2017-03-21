3182f378966fb968b3c9b47c04a39fe432dfe5b6
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-21 13:26:01 +0100

    coding-hours.py added

---
+++ week02/day_2/coding-hours.py
@@ -0,0 +1,10 @@
+__author__ = 'ylwoi'
+codeHours = 6
+semesterWeeks = 17
+semesterCodingHours = semesterWeeks*5*codeHours
+print(semesterCodingHours, "hours of coding spent on a semester.")
+
+codeHoursWeek = codeHours*5
+myCodeHoursWeek = 52
+percentage = myCodeHoursWeek / codeHoursWeek *100
+print("If you have", myCodeHoursWeek, "hours coding in a week then your work percentage is", percentage, "%")
