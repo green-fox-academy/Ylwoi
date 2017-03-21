51911873e61146fbdf3c09d8e8e6ec55f9c1f87b
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-21 11:48:40 +0100

    humpty-dumpty.py added

---
+++ week02/day_2/humpty-dumpty.py
@@ -0,0 +1,5 @@
+__author__ = 'ylwoi'
+print("All the king's horses and all the king's men")
+print("Humpty Dumpty had a great fall.")
+print("Humpty Dumpty sat on a wall,")
+print("Couldn't put Humpty together again.")
