63d7b40d4d60d02528bafcb71c096ac96b603047
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-21 17:38:19 +0100

    count-from-to.py added done

---
+++ week02/day_2/count-from-to.py
@@ -0,0 +1,25 @@
+__author__ = 'ylwoi'
+
+# Create a program that asks for two numbers
+# If the second number is not bigger than the first one it should print:
+# "The second number should be bigger"
+#
+# If it is bigger it should count from the first number to the second by one
+#
+# example:
+#
+# first number: 3, second number: 6, should print:
+#
+# 3
+# 4
+# 5
+
+num1 = int(input("First number:"))
+num2 = int(input("Second number:"))
+
+if num2 <= num1:
+    print("The second number should be bigger")
+else:
+    for i in range(num1, num2):
+        print(num1)
+        num1 += 1
