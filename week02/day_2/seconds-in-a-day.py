1f503a560322f238461b23879c739613975ca049
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-21 14:06:44 +0100

    seconds-in-a-day.py added

---
+++ week02/day_2/seconds-in-a-day.py
@@ -0,0 +1,6 @@
+__author__ = 'ylwoi'
+current_hours = 14
+current_minutes = 34
+current_seconds = 42
+remainingSecs = (24-current_hours)*3600 + (60-current_minutes)*60 + (60-current_seconds)
+print("Remaining seconds:", remainingSecs)
