ec839df792de14941a48700d3188b8363722c4eb
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-22 13:21:08 +0100

    change_element.py added, change list element, done

---
+++ week02/day_3/change_element.py
@@ -0,0 +1,10 @@
+__author__ = 'ylwoi'
+
+# - Create an array variable named `s`
+#   with the following content: `[1, 2, 3, 8, 5, 6]`
+# - Change the 8 to 4
+# - Print the fourth element
+
+a = [1, 2, 3, 8, 5, 6]
+a[3] = 4
+print(a[3])
