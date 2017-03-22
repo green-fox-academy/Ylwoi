7c25430d4ff7d5e54525c95f7f4aa7243701bbf4
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-22 17:55:42 +0100

    diagonal matrix fixed to dynamic list

--- week02/day_3/diagonal_matrix.py
+++ week02/day_3/diagonal_matrix.py
@@ -9,15 +9,14 @@ __author__ = 'ylwoi'
 #   0 0 0 1
 #
 # - Print this two dimensional list to the output
+y = int(input("Size of two dimensional list:"))
+matrix = [[0 for x in range(y)] for x in range(y)]
 
-matrix = [[0 for x in range(4)] for x in range(4)]
-
-for i in range(0, 4):
-    for j in range(0, 4):
+for i in range(0, y):
+    for j in range(0, y):
         if i == j:
             matrix[i][j] = 1
-        if j == 3:
+        if j == y-1:
             print(matrix[i][j])
         else:
             print(matrix[i][j], end=" ")
-
