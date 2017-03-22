63052c3580cfa8679d6621fe7957009ff5739a7f
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-22 11:05:56 +0100

    doubling added and works

---
+++ week02/day_3/doubling.py
@@ -0,0 +1,12 @@
+__author__ = 'ylwoi'
+
+# - Create a variable named `ak` and assign the value `123` to it
+# - Create a function called `doubling` that doubles it's input parameter
+# - Print the result of `doubling(ak)`
+
+ak = 123
+
+def doubling(x):
+    return x*2
+
+print(doubling(ak))
