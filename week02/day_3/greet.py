3b6414891c00473dd867d4f624732d09091af40a
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-22 11:12:36 +0100

    greet.py added, the greeting function works well

---
+++ week02/day_3/greet.py
@@ -0,0 +1,13 @@
+__author__ = 'ylwoi'
+
+# - Create variable named `al` and assign the value `Greenfox` to it
+# - Create a function called `greet` that greets it's input parameter
+#     - Greeting is printing e.g. `Greetings, dear Greenfox`
+# - Greet `al`
+
+al = "Greenfox"
+
+def greet(x):
+    print("Greetings, dear " + str(x))
+
+greet(al)
