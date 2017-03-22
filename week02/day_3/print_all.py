d54968f2fb115d63b1282e62a029aeaff8f5f93c
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-22 13:29:44 +0100

    print_all.py added,prints all elements from the list, done

---
+++ week02/day_3/print_all.py
@@ -0,0 +1,9 @@
+__author__ = 'ylwoi'
+
+# - Create a variable named `af`
+#   with the following content: `[4, 5, 6, 7]`
+# - Print all the elements of `af`
+
+af = [4, 5, 6, 7]
+for i in range(0, len(af)):
+    print(af[i])
