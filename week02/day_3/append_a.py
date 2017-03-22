4096bea16866c19aeff82164ce6198c7f07ef9c2
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-22 11:26:11 +0100

    gitignore file added

---
+++ week02/day_3/append_a.py
@@ -0,0 +1,14 @@
+__author__ = 'ylwoi'
+
+# - Create a variable named `am` and assign the value `kuty` to it
+# - Write a function called `appendA` that gets a string as an input
+#   and appends an 'a' character to its end
+# - Print the result of `appendA(am)`
+
+am = 'kuty'
+
+def appendA(text):
+    text += 'a'
+    return text
+
+print(appendA(am))
