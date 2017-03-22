21a48843fe819f69faee36c49f5ef5661ac5d4c6
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-22 15:06:07 +0100

    append_a_list.py added, added "a" to all list elements, done

---
+++ week02/day_3/append_a_list.py
@@ -0,0 +1,10 @@
+__author__ = 'ylwoi'
+
+# - Create a variable named `nimals`
+#   with the following content: `["kuty", "macs", "cic"]`
+# - Add all elements an `"a"` at the end
+
+nimals = ["kuty", "macs", "cic"]
+for i in range(len(nimals)):
+    nimals[i] += "a"
+print(nimals)
