e69b7d1dcc20b87d651c39a9b9485e37f77fbcd6
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-23 12:49:00 +0100

    reverse.py added, reverses the string

---
+++ week02/day_4/reverse.py
@@ -0,0 +1,7 @@
+__author__ = 'ylwoi'
+
+# Reverse the string using StringBuilder
+
+reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+
+print(reversed[::-1])
