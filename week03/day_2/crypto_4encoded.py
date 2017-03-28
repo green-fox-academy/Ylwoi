aa64a80aa3148c6b036ff0ceb4c2b9012b002025
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-28 12:57:27 +0100

    crypto_4encoded.py added, feat( the function changes all characters  ascii value by -1 )

---
+++ week03/day_2/crypto_4encoded.py
@@ -0,0 +1,21 @@
+__author__ = 'ylwoi'
+
+#help for char coding
+#ch = 't'
+#ch2 = ord(ch)+1
+#print(chr(ch2))
+
+def decrypt(filename):
+    my_file = open(filename, mode="r")
+    list1 = my_file.readlines()
+    encoded = ''
+    for i in list1:
+        print("")
+        for j in i:
+            if j ==' ':
+                print(" ", end='')
+            else:
+                encoded = ord(j)-1
+                print(chr(encoded), end="")
+
+decrypt("encoded-lines.txt")
