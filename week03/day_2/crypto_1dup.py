74641513f8054fafc9dde7024fee8e0693f90ae8
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-28 14:45:58 +0200

    feat(line breaks deleted, file close method added)

--- week03/day_2/crypto_1dup.py
+++ week03/day_2/crypto_1dup.py
@@ -3,6 +3,7 @@ __author__ = 'ylwoi'
 def decrypt(filename):
     my_file = open(filename, mode="r")
     for line in my_file:
-        print(line[::2])
+        print(line[::2], end="")
+    my_file.close()
 
 decrypt("duplicated-chars.txt")
