69007f577f41617451f25b7ef0b2e0670cbde44a
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-29 21:58:34 +0200

    feat(almost reproduce the triangles) con(have to fix the triangles size)

---
+++ week03/day_3/triangles.py
@@ -0,0 +1,22 @@
+__author__ = 'ylwoi'
+
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+# reproduce this:
+# [https://github.com/greenfox-academy/teaching-materials/blob/master/exercises/drawing/triangles/r5.png]
+
+start = 0
+finish = 300
+for lines in range(20):
+    for i in range(start, finish, 20):
+        canvas.create_polygon(i, finish-10, i+10, finish-20, i + 20, finish-10, outline='black', fill='white')
+    start += 10
+    finish -= 10
+
+
+root.mainloop()
