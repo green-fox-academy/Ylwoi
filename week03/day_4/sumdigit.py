fde71cc1e52e531ad26d8bc15b4f864acd7a6d36
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-30 10:57:22 +0200

    feat(returns the sum of the given numbers digits recursively)

---
+++ week03/day_4/sumdigit.py
@@ -0,0 +1,13 @@
+__author__ = 'ylwoi'
+
+# Given a non-negative int n, return the sum of its digits recursively (no loops).
+# Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while
+# divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+def sumdigit(number):
+    if number < 10:
+        return number
+    else:
+        return (number % 10) + sumdigit(number//10)
+
+print(sumdigit(126))
