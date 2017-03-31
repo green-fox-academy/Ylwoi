e977ab74eac78c643a2041640451ca1b5c2a3e37
Horvath Balazs <balazs.horvath989@gmail.com>
2017-03-31 11:50:57 +0200

    feat(three hexagon in a hexagon fractal)

---
+++ week03/day_4/graphic2.py
@@ -0,0 +1,26 @@
+__author__ = 'ylwoi'
+
+from tkinter import *
+import time
+
+root = Tk()
+canvas = Canvas(root, width='600', height='600')
+canvas.pack()
+
+#canvas.create_polygon( outline='black', width='1')
+fv = (3**0.5)/2
+
+def draw_hexagon(x, y, size):
+    canvas.create_polygon(x+size/2, y-size*fv, x-size/2, y-size*fv, x-size, y, x-size/2, y+size*fv, x+size/2, y+size*fv, x+size, y, outline='black', width='1', fill='')
+
+def recursive(x, y, size):
+    draw_hexagon(x, y, size)
+    if size > 10:
+        recursive(x-size*fv/3.46, y-size*fv/2, size/2)
+        recursive(x-size*fv/3.46, y+size*fv/2, size/2)
+        recursive(x+size/2, y, size/2)
+
+
+recursive(300, 300, 300)
+
+root.mainloop()
