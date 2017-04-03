ca2b4bafc64d97b1a5f78e49064fc4506eb8397d
Horvath Balazs <balazs.horvath989@gmail.com>
2017-04-03 12:50:57 +0200

    using two classes to make the correct output

---
+++ week04/day_1/fleet_of_things.py
@@ -0,0 +1,49 @@
+__author__ = 'ylwoi'
+
+class Fleet:
+    def __init__(self):
+        self.things = []
+
+    def add(self, thing):
+        self.things.append(thing)
+
+    def __str__(self):
+        result = ""
+        for i in range(0, len(self.things)):
+            result += str(i+1) + ". " + self.things[i].__str__() + "\n"
+        return result
+
+
+class Thing:
+    def __init__(self, name):
+        self.name = name
+        self.completed = False
+
+    def complete(self):
+        self.completed = True
+
+    def __str__(self):
+        return ("[x] " if self.completed else "[ ] ") + self.name
+
+
+fleet = Fleet()
+# Create a fleet of things to have this output:
+# 1. [ ] Get milk
+# 2. [ ] Remove the obstacles
+# 3. [x] Stand up
+# 4. [x] Eat lunch
+
+thing1 = Thing('Get milk')
+thing2 = Thing('Remove the obstacles')
+thing3 = Thing('Stand up')
+thing4 = Thing('Eat lunch')
+
+thing3.complete()
+thing4.complete()
+
+fleet.add(thing1)
+fleet.add(thing2)
+fleet.add(thing3)
+fleet.add(thing4)
+
+print(fleet)
