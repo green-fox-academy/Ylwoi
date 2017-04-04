8cb7b0c8e8f8b366da812c9e3034c8fa4f330990
Horvath Balazs <balazs.horvath989@gmail.com>
2017-04-04 11:39:40 +0200

    feat(student class added, inherits from Person some args and funcs)

--- week04/day_2/inheritence_exercise.py
+++ week04/day_2/inheritence_exercise.py
@@ -10,9 +10,30 @@ class Person():
     def get_goal(self):
         print("My goal is: Live for the moment!")
 
+class Student(Person):
+    def __init__(self, name='Jane Doe', age=30, gender='female', previous_organization='The School of Life', skipped_days=0):
+        super().__init__(name, age, gender)
+        self.previous_organization = previous_organization
+        self.skipped_days = 0
+    def get_goal(self):
+        print('Be a junior software developer.')
+    def introduce(self):
+        super().introduce()
+        print(" from", self.previous_organization, "who skipped", self.skipped_days, "days from the course already.")
+    def skip_days(self, number_of_days):
+        self.number_of_days = number_of_days
+        self.skipped_days += number_of_days
+
+
 
 std_pers = Person()
 vili = Person('Vilmos', 61, 'male')
-
 vili.introduce()
 std_pers.introduce()
+
+std_student = Student()
+dexter = Student('Dexter', 20, 'male', 'Fox school')
+dexter.get_goal()
+std_student.skip_days(3)
+dexter.introduce()
+std_student.introduce()
