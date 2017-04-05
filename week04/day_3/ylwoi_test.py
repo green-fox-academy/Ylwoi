12fa9d7ac553a4b4bd78e690f9ee5f2c58f995f4
Horvath Balazs <balazs.horvath989@gmail.com>
2017-04-05 13:53:35 +0200

    test(my_work): TestSum  class, tests the sum method with one, more, none numbers

--- week04/day_3/ylwoi_test.py
+++ week04/day_3/ylwoi_test.py
@@ -1,7 +1,7 @@
 __author__ = 'ylwoi'
 
 import unittest
-from ylwoi_work import Apples
+from ylwoi_work import Apples, Sum
 
 
 class TestApplesMethods(unittest.TestCase):
@@ -9,6 +9,17 @@ class TestApplesMethods(unittest.TestCase):
         apple = Apples()
         self.assertEqual(apple.get_apple(), 'apple')
 
+
+class TestSum(unittest.TestCase):
+    def test_sum_method_one_number(self):
+        sum_nums = Sum()
+        self.assertEqual(sum_nums.sum([3]), 3)
+    def test_sum_method_more_number(self):
+        sum_nums = Sum()
+        self.assertEqual(sum_nums.sum([3, 5, 1]), 9)
+    def test_sum_method_without_number(self):
+        sum_nums = Sum()
+        self.assertEqual(sum_nums.sum([]), 0)
 
 
 
