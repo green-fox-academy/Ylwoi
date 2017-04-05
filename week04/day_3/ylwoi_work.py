4430b87c226743e304b0e4e54629537531cdba5a
Horvath Balazs <balazs.horvath989@gmail.com>
2017-04-05 14:29:25 +0200

    feat(my_work): class Anagram created, method returns true if the two words are anagrams

--- week04/day_3/ylwoi_work.py
+++ week04/day_3/ylwoi_work.py
@@ -14,3 +14,17 @@ class Sum:
             sum_of_nums += i
         return sum_of_nums
 
+
+class Anagram:
+    def anagram(self, word1, word2):
+        self.word1 = word1
+        self.word2 = word2
+        list_word1 = list(sorted(self.word1))
+        list_word2 = list(sorted(self.word2))
+        if len(list_word1) == len(list_word2):
+            for i in range(len(list_word1)):
+                if list_word1[i] != list_word2[i]:
+                    return False
+            return True
+        else:
+            return False
