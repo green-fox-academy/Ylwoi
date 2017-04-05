a2a0e607960f24463bde2e7f97fb6bb180999729
Horvath Balazs <balazs.horvath989@gmail.com>
2017-04-05 13:44:19 +0200

    feat(my_work): Sum  class, sum method created, returns the sums of the list

--- week04/day_3/ylwoi_work.py
+++ week04/day_3/ylwoi_work.py
@@ -4,3 +4,14 @@ __author__ = 'ylwoi'
 class Apples:
     def get_apple(self):
         return 'apple'
+
+
+class Sum:
+    number_list = [3, 5, 1]
+
+    def sum(self, list=[]):
+        self.numbers = list
+        sum_of_nums = 0
+        for i in self.numbers:
+            sum_of_nums += i
+        return sum_of_nums
