549de5494b065e0c949092058150a75bd2bfd6de
Horvath Balazs <balazs.horvath989@gmail.com>
2017-04-11 15:10:18 +0200

    wall drawer method created

--- week05/WandererGame/main.py
+++ week05/WandererGame/main.py
@@ -15,6 +15,16 @@ class Map(object):
             for draw_column in range(10):
                 canvas.create_image(self.tileX+draw_row*72, self.tileY+draw_column*72, image=self.floor_pic)
 
+    def draw_wall(self, canvas):
+        wall_matrix = [
+            [3, 5], [3, 5, 7, 8], [1, 2, 3, 5, 7, 8], [5], [0, 1, 2, 3, 5, 6, 7, 8],
+            [1, 3, 8], [1, 3, 5, 6, 8], [5, 6, 8], [1, 2, 3, 8], [3, 5, 6, 8], [1, 3, 5]
+        ]
+
+        for i in wall_matrix:
+            for j in i:
+               canvas.create_image(self.tileX + j*72, self.tileY + wall_matrix.index(i)*72, image=self.wall_pic)
+
 
 class Hero(object):
     def __init__(self):
@@ -26,8 +36,7 @@ class Hero(object):
         self.hero_pic_right = PhotoImage(file='assets\hero-right.png')
 
     def draw_hero(self, canvas, hero_picture):
-        #canvas.delete()
-        canvas.create_image(self.heroX, self.heroY, image=hero_picture)
+        self.a = canvas.create_image(self.heroX, self.heroY, image=hero_picture)
 
 # Create the tk environment as usual
 root = Tk()
@@ -40,6 +49,7 @@ hero = Hero()
 # Create a function that can be called when a key pressing happens
 def on_key_press(e):
     # When the keycode is 111 (up arrow) we move the position of our box higher
+    canvas.delete(hero.a)
     if e.keysym == 'Up':
         hero.heroY = hero.heroY - 72
         hero.draw_hero(canvas, hero.hero_pic_up)
@@ -58,7 +68,6 @@ def on_key_press(e):
 # Tell the canvas that we prepared a function that can deal with the key press events
 canvas.bind("<KeyPress>", on_key_press)
 
-game_map.draw_floor(canvas)
 canvas.pack()
 
 # Select the canvas to be in focused so it actually recieves the key hittings
@@ -66,6 +75,7 @@ canvas.focus_set()
 
 # Draw the box in the initial position
 game_map.draw_floor(canvas)
+game_map.draw_wall(canvas)
 hero.draw_hero(canvas, hero.hero_pic_down)
 
 root.mainloop()
