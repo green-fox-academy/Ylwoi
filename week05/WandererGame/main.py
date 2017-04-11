0b03e292139fb4838f659565f30bc0b27a17da0b
Horvath Balazs <balazs.horvath989@gmail.com>
2017-04-11 16:39:20 +0200

    move control method created to analyze the hero and walls coords

--- week05/WandererGame/main.py
+++ week05/WandererGame/main.py
@@ -9,6 +9,10 @@ class Map(object):
         self.tileY = 36
         self.floor_pic = PhotoImage(file='assets\Floor.png')
         self.wall_pic = PhotoImage(file='assets\wall.png')
+        self.wall_matrix = [
+            [3, 5], [3, 5, 7, 8], [1, 2, 3, 5, 7, 8], [5], [0, 1, 2, 3, 5, 6, 7, 8],
+            [1, 3, 8], [1, 3, 5, 6, 8], [5, 6, 8], [1, 2, 3, 8], [3, 5, 6, 8], [1, 3, 5]
+        ]
 
     def draw_floor(self, canvas):
         for draw_row in range(10):
@@ -16,14 +20,9 @@ class Map(object):
                 canvas.create_image(self.tileX+draw_row*72, self.tileY+draw_column*72, image=self.floor_pic)
 
     def draw_wall(self, canvas):
-        wall_matrix = [
-            [3, 5], [3, 5, 7, 8], [1, 2, 3, 5, 7, 8], [5], [0, 1, 2, 3, 5, 6, 7, 8],
-            [1, 3, 8], [1, 3, 5, 6, 8], [5, 6, 8], [1, 2, 3, 8], [3, 5, 6, 8], [1, 3, 5]
-        ]
-
-        for i in wall_matrix:
+        for i in self.wall_matrix:
             for j in i:
-               canvas.create_image(self.tileX + j*72, self.tileY + wall_matrix.index(i)*72, image=self.wall_pic)
+               canvas.create_image(self.tileX + j*72, self.tileY + self.wall_matrix.index(i)*72, image=self.wall_pic)
 
 
 class Hero(object):
@@ -43,13 +42,18 @@ root = Tk()
 canvas = Canvas(root, width=720, height=720)
 
 # Create a function that can be called when a key pressing happens
-class GameLogic:
+class GameLogic(object):
     def __init__(self):
         canvas.bind("<KeyPress>", self.on_key_press)
 
-    #def game_logic(self):
-
-
+    def move_control(self):
+        self.hero_cord_x = hero.heroX // 72
+        self.hero_cord_y = hero.heroY // 72
+        for i in game_map.wall_matrix:
+            for j in i:
+                if self.hero_cord_x == j and self.hero_cord_y == game_map.wall_matrix.index(i):
+                    return False
+        return True
 
     def on_key_press(self, e):
         # When the keycode is 111 (up arrow) we move the position of our box higher
@@ -57,16 +61,35 @@ class GameLogic:
         canvas.delete(hero.a)
         if self.e.keysym == 'Up':
             hero.heroY = hero.heroY - 72
-            hero.draw_hero(canvas, hero.hero_pic_up)
+            if logic.move_control() == False:
+                hero.heroY = hero.heroY + 72
+                hero.draw_hero(canvas, hero.hero_pic_up)
+            else:
+                hero.draw_hero(canvas, hero.hero_pic_up)
+
         elif self.e.keysym == 'Down':
             hero.heroY = hero.heroY + 72
-            hero.draw_hero(canvas, hero.hero_pic_down)
+            if logic.move_control() == False:
+                hero.heroY = hero.heroY - 72
+                hero.draw_hero(canvas, hero.hero_pic_up)
+            else:
+                hero.draw_hero(canvas, hero.hero_pic_down)
+
         elif self.e.keysym == 'Left':
             hero.heroX = hero.heroX - 72
-            hero.draw_hero(canvas, hero.hero_pic_left)
+            if logic.move_control() == False:
+                hero.heroX = hero.heroX + 72
+                hero.draw_hero(canvas, hero.hero_pic_up)
+            else:
+                hero.draw_hero(canvas, hero.hero_pic_left)
+
         elif self.e.keysym == 'Right':
             hero.heroX = hero.heroX + 72
-            hero.draw_hero(canvas, hero.hero_pic_right)
+            if logic.move_control() == False:
+                hero.heroX = hero.heroX - 72
+                hero.draw_hero(canvas, hero.hero_pic_up)
+            else:
+                hero.draw_hero(canvas, hero.hero_pic_right)
         # and lower if the key that was pressed the down arrow
         # draw the box again in the new position
 
@@ -76,8 +99,6 @@ class GameLogic:
 game_map = Map()
 hero = Hero()
 logic = GameLogic()
-
-
 
 
 canvas.pack()
