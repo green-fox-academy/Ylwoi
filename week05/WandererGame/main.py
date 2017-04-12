4eed70d88fda7a93eade5ea91657ffdee3e0799d
Horvath Balazs <balazs.horvath989@gmail.com>
2017-04-12 09:32:29 +0200

    gamelogic refactored, skeleton class created

--- week05/WandererGame/main.py
+++ week05/WandererGame/main.py
@@ -16,7 +16,7 @@ class Map(object):
 
     def draw_floor(self, canvas):
         for draw_row in range(10):
-            for draw_column in range(10):
+            for draw_column in range(11):
                 canvas.create_image(self.tileX+draw_row*72, self.tileY+draw_column*72, image=self.floor_pic)
 
     def draw_wall(self, canvas):
@@ -37,21 +37,30 @@ class Hero(object):
     def draw_hero(self, canvas, hero_picture):
         self.a = canvas.create_image(self.heroX, self.heroY, image=hero_picture)
 
+
+class Skeleton(object):
+    def __init__(self, x, y):
+        self.skeleton_pic = PhotoImage(file='assets\skeleton.png')
+        self.skeletonX = x
+        self.skeletonY = y
+
+    def draw_skeleton(self, canvas):
+        self.ds = canvas.create_image(self.skeletonX, self.skeletonY, image=self.skeleton_pic)
 # Create the tk environment as usual
 root = Tk()
-canvas = Canvas(root, width=720, height=720)
+canvas = Canvas(root, width=720, height=792)
 
 # Create a function that can be called when a key pressing happens
 class GameLogic(object):
     def __init__(self):
         canvas.bind("<KeyPress>", self.on_key_press)
 
-    def move_control(self):
-        self.hero_cord_x = hero.heroX // 72
-        self.hero_cord_y = hero.heroY // 72
+    def move_control(self, x=0, y=0):
+        self.hero_cord_x = (hero.heroX+x) // 72
+        self.hero_cord_y = (hero.heroY+y) // 72
         if self.hero_cord_x < 0 or self.hero_cord_x > 9:
             return False
-        if self.hero_cord_y < 0 or self.hero_cord_y > 9:
+        if self.hero_cord_y < 0 or self.hero_cord_y > 10:
             return False
         for i in game_map.wall_matrix:
             for j in i:
@@ -60,38 +69,33 @@ class GameLogic(object):
         return True
 
     def on_key_press(self, e):
-        # When the keycode is 111 (up arrow) we move the position of our box higher
         self.e = e
         canvas.delete(hero.a)
         if self.e.keysym == 'Up':
-            hero.heroY = hero.heroY - 72
-            if logic.move_control() == False:
-                hero.heroY = hero.heroY + 72
+            if logic.move_control(0,-72) == True:
+                hero.heroY = hero.heroY - 72
                 hero.draw_hero(canvas, hero.hero_pic_up)
             else:
                 hero.draw_hero(canvas, hero.hero_pic_up)
 
         elif self.e.keysym == 'Down':
-            hero.heroY = hero.heroY + 72
-            if logic.move_control() == False:
-                hero.heroY = hero.heroY - 72
-                hero.draw_hero(canvas, hero.hero_pic_up)
+            if logic.move_control(0,72) == True:
+                hero.heroY = hero.heroY + 72
+                hero.draw_hero(canvas, hero.hero_pic_down)
             else:
                 hero.draw_hero(canvas, hero.hero_pic_down)
 
         elif self.e.keysym == 'Left':
-            hero.heroX = hero.heroX - 72
-            if logic.move_control() == False:
-                hero.heroX = hero.heroX + 72
-                hero.draw_hero(canvas, hero.hero_pic_up)
+            if logic.move_control(-72, 0) == True:
+                hero.heroX = hero.heroX - 72
+                hero.draw_hero(canvas, hero.hero_pic_left)
             else:
                 hero.draw_hero(canvas, hero.hero_pic_left)
 
         elif self.e.keysym == 'Right':
-            hero.heroX = hero.heroX + 72
-            if logic.move_control() == False:
-                hero.heroX = hero.heroX - 72
-                hero.draw_hero(canvas, hero.hero_pic_up)
+            if logic.move_control(72,0) == True:
+                hero.heroX = hero.heroX + 72
+                hero.draw_hero(canvas, hero.hero_pic_right)
             else:
                 hero.draw_hero(canvas, hero.hero_pic_right)
         # and lower if the key that was pressed the down arrow
@@ -103,6 +107,7 @@ class GameLogic(object):
 game_map = Map()
 hero = Hero()
 logic = GameLogic()
+skel_1 = Skeleton(108,36 )
 
 
 canvas.pack()
@@ -114,6 +119,7 @@ canvas.focus_set()
 game_map.draw_floor(canvas)
 game_map.draw_wall(canvas)
 hero.draw_hero(canvas, hero.hero_pic_down)
+skel_1.draw_skeleton(canvas)
 
 root.mainloop()
 
