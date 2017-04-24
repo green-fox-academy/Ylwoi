23e050593bdcdb8d05b35b1ad61f469e358d03b0
Horvath Balazs <balazs.horvath989@gmail.com>
2017-04-24 17:15:13 +0200

    tictactoe: html, css, js, files added

---
+++ week06/day4/TicTacToe/script.js
@@ -0,0 +1,74 @@
+/**
+ * Created by cw on 2017-04-21.
+ */
+document.querySelector(".i input").addEventListener("keydown", function(e)
+{
+   console.log(e)
+    if (e.key != "x" && e.key != "o" && e.key != "Backspace" && e.key != "Tab") {
+        e.preventDefault()
+    }
+})
+
+document.querySelector(".ii input").addEventListener("keydown", function(e)
+{
+   console.log(e)
+    if (e.key != "x" && e.key != "o"&& e.key != "Backspace" && e.key != "Tab") {
+        e.preventDefault()
+    }
+})
+
+document.querySelector(".iii input").addEventListener("keydown", function(e)
+{
+   console.log(e)
+    if (e.key != "x" && e.key != "o" && e.key != "Backspace" && e.key != "Tab") {
+        e.preventDefault()
+    }
+})
+
+document.querySelector(".vi input").addEventListener("keydown", function(e)
+{
+   console.log(e)
+    if (e.key != "x" && e.key != "o" && e.key != "Backspace" && e.key != "Tab") {
+        e.preventDefault()
+    }
+})
+
+document.querySelector(".v input").addEventListener("keydown", function(e)
+{
+   console.log(e)
+    if (e.key != "x" && e.key != "o" && e.key != "Backspace" && e.key != "Tab") {
+        e.preventDefault()
+    }
+})
+
+document.querySelector(".vi input").addEventListener("keydown", function(e)
+{
+   console.log(e)
+    if (e.key != "x" && e.key != "o" && e.key != "Backspace" && e.key != "Tab") {
+        e.preventDefault()
+    }
+})
+
+document.querySelector(".vii input").addEventListener("keydown", function(e)
+{
+   console.log(e)
+    if (e.key != "x" && e.key != "o" && e.key != "Backspace" && e.key != "Tab") {
+        e.preventDefault()
+    }
+})
+
+document.querySelector(".viii input").addEventListener("keydown", function(e)
+{
+   console.log(e)
+    if (e.key != "x" && e.key != "o" && e.key != "Backspace" && e.key != "Tab") {
+        e.preventDefault()
+    }
+})
+
+document.querySelector(".ix input").addEventListener("keydown", function(e)
+{
+   console.log(e)
+    if (e.key != "x" && e.key != "o" && e.key != "Backspace" && e.key != "Tab") {
+        e.preventDefault()
+    }
+})
