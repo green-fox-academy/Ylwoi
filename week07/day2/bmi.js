0a27850c273accb3fb332cfa4555abd0dfa5bf29
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-02 14:17:39 +0200

    prints out bmi value

---
+++ week07/day2/bmi.js
@@ -0,0 +1,11 @@
+/**
+ * Created by cw on 2017-05-02.
+ */
+
+'use strict';
+
+var massInKg = 81.2;
+var heightInM = 1.78;
+
+// Print the Body mass index (BMI) based on these values
+console.log('The BMI value is:', massInKg/Math.pow(heightInM,2));
