0b9e6e40f5433b8b0f4a1fff57856bc5b46297bf
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-02 15:34:09 +0200

    prints out even numbers between 0 and 500

---
+++ week07/day2/print-even.js
@@ -0,0 +1,9 @@
+/**
+ * Created by cw on 2017-05-02.
+ */
+'use strict';
+
+// Create a program that prints all the even numbers between 0 and 500
+for (var i = 2; i <= 500; i+=2) {
+    console.log(i);
+}
