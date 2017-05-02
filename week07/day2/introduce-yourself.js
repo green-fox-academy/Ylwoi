f542e388d9192cca83c1786a3aa9b3a5d3aafafa
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-02 13:32:40 +0200

    prints out my few parameters

---
+++ week07/day2/introduce-yourself.js
@@ -0,0 +1,23 @@
+/**
+ * Created by cw on 2017-05-02.
+ */
+'use strict';
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+//  Example output:
+//  John Doe
+//  31
+//  1.87
+
+var name = 'Balazs';
+var age = 28;
+var height = 2.00;
+
+console.log(name);
+console.log(age);
+console.log(height);
