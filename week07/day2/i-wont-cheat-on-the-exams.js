4eca3c675da278a693894c49225ad3936d68c7bb
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-02 15:30:44 +0200

    prints out a sentence 100 times

---
+++ week07/day2/i-wont-cheat-on-the-exams.js
@@ -0,0 +1,11 @@
+/**
+ * Created by cw on 2017-05-02.
+ */
+
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+for (var i = 0; i <100; i++) {
+    console.log('I won\'t cheat on the exam!')
+}
