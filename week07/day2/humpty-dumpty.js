4592568009872cd8da4893a1d7350232b86f3aa5
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-02 13:25:27 +0200

    modified to print correctly

---
+++ week07/day2/humpty-dumpty.js
@@ -0,0 +1,11 @@
+/**
+ * Created by cw on 2017-05-02.
+ */
+
+'use strict';
+
+// Modify this program to console.log Humpty Dumpty riddle correctly
+console.log('Humpty Dumpty had a great fall.');
+console.log('Humpty Dumpty sat on a wall,');
+console.log('All the king\'s horses and all the king\'s men');
+console.log('Couldn\'t put Humpty together again.');
