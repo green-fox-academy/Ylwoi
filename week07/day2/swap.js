3bfdaacf3443e4472177039a6a8878474f761ff0
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-02 14:35:10 +0200

    swaps two values

---
+++ week07/day2/swap.js
@@ -0,0 +1,13 @@
+/**
+ * Created by cw on 2017-05-02.
+ */
+
+'use strict';
+
+var a = 123;
+var b = 526;
+// Swap the values of these variables
+b = [a, a = b][0];
+
+console.log(a);
+console.log(b);
