dd00d868b081924d16e7fc75611e65bdd7536098
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-02 14:12:35 +0200

    prints out basic infos

---
+++ week07/day2/define-basic-info.js
@@ -0,0 +1,16 @@
+/**
+ * Created by cw on 2017-05-02.
+ */
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+var name = 'Balazs';
+var age = 28;
+var height = 2.00;
+var married = false;
+console.log('Name:',name,'Age:', age,'Height:', height,'Married:', married);
