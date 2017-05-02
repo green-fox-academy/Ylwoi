7c0ebcd510323feb6b4a9955a1e2eb5a7aa9c0f3
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-02 13:18:25 +0200

    Greets 3 persons

---
+++ week07/day2/hello-others.js
@@ -0,0 +1,14 @@
+/**
+ * Created by cw on 2017-05-02.
+ */
+// Greet 3 of your classmates with this program, in three separate lines
+// like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+
+'use strict';
+console.log('Hello, Esther!');
+console.log('Hello, Mary!');
+console.log('Hello, Joe!');
