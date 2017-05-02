620bb75981b139e84e3d0f394dd45d9f7cabab4e
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-02 13:36:48 +0200

    logs out operations of two numbers

---
+++ week07/day2/two-numbers.js
@@ -0,0 +1,26 @@
+/**
+ * Created by cw on 2017-05-02.
+ */
+
+'use strict';
+
+// Create a program that prints a few operations on two numbers: 22 and 13
+
+// Print the result of 13 added to 22
+
+// Print the result of 13 substracted from 22
+
+// Print the result of 22 mutiplied by 13
+
+// Print the result of 22 divided by 13 (as a decimal fraction)
+
+// Print the reminder of 22 divided by 13
+
+var a = 22;
+var b = 13;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
