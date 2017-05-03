6f8159be3e66e53d8077092dd2b3a7c996b66c62
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-03 13:30:20 +0200

    logs out the third element of the array

---
+++ week07/day3/third.js
@@ -0,0 +1,9 @@
+/**
+ * Created by cw on 2017-05-03.
+ */
+'use strict';
+// - Create a variable named `magicNumbers`
+//   with the following content: `[1, 3, 5, 7]`
+// - Print the third element of `magicNumbers`
+var magicNumbers = [1, 3, 5, 7];
+console.log(magicNumbers[2]);
