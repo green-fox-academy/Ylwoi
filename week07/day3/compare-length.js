8150ad0a7e206ae3fe4b39cda289e66bf900a209
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-03 13:35:40 +0200

    log to the console which array has more elements

---
+++ week07/day3/compare-length.js
@@ -0,0 +1,16 @@
+/**
+ * Created by cw on 2017-05-03.
+ */
+'use strict';
+// - Create a variable named `p1`
+//   with the following content: `[1, 2, 3]`
+// - Create a variable named `p2`
+//   with the following content: `[4, 5]`
+// - Log to the console if `p2` has more elements than `p1`
+var p1 = [1, 2, 3];
+var p2 = [4, 5];
+if (p2.length > p1.length){
+    console.log('p2 is more');
+} else {
+    console.log('p2 is less');
+}
