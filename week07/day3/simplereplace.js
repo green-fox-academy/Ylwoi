6c2d9f061380da58d04b9ca62297691a76b8ffe7
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-03 16:19:03 +0200

    replaces the ordered word in the string

---
+++ week07/day3/simplereplace.js
@@ -0,0 +1,14 @@
+/**
+ * Created by cw on 2017-05-03.
+ */
+'use strict';
+
+var example = "In a dishwasher far far away";
+
+// I would like to replace "dishwasher" with "galaxy" in this example
+// Please fix it for me!
+// Expected ouput: In a galaxy far far away
+
+example = example.replace('dishwasher', 'galaxy');
+
+console.log(example);
