63c9dfe0133bfa70d5cba43528ac378562517dcc
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-03 16:30:38 +0200

    reverse the string

---
+++ week07/day3/reverseString.js
@@ -0,0 +1,10 @@
+/**
+ * Created by cw on 2017-05-03.
+ */
+
+'use strict';
+// Reverse the string!
+
+var reversed = '.eslaf eb t\'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI';
+reversed = reversed.split('').reverse().join('');
+console.log(reversed);
