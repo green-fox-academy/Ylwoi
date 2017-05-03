8363c607bf05ca6cdc5db786bed41841640a0244
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-03 17:12:25 +0200

    fixes the url to work

---
+++ week07/day3/urlfixer.js
@@ -0,0 +1,11 @@
+/**
+ * Created by cw on 2017-05-03.
+ */
+'use strict';
+// Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+// Also, the URL is missing a crutial component, find out what it is and insert it too!
+
+var url = "https//www.reddit.com/r/nevertellmethebots";
+url = url.replace('https','https:');
+
+console.log(url);
