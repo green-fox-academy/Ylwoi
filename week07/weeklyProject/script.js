4fa7ff26335f488774fff02e512cf86ea5986967
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-05 10:15:35 +0200

    feat(js): picture list created

--- week07/weeklyProject/script.js
+++ week07/weeklyProject/script.js
@@ -2,3 +2,47 @@
  * Created by cw on 2017-05-04.
  */
 'use strict';
+
+var picList = [
+    {
+        img:'img/Fox.jpg',
+        title: 'The Fox looks back',
+        description: 'Lorem ipsum dolor sit amet, libris iisque scripta et mel. Autem definiebas philosophia ea vis. Eos sanctus omittam molestiae at, eius viris audiam at per. Tale labitur virtute nam in, ei cum hinc ridens efficiendi. Vis luptatum voluptaria in.'
+    },
+    {
+        img:'img/frog.jpg',
+        title: 'Friggy Froggy',
+        description: 'Lorem ipsum dolor sit amet, libris iisque scripta et mel. Autem definiebas philosophia ea vis. Eos sanctus omittam molestiae at, eius viris audiam at per. Tale labitur virtute nam in, ei cum hinc ridens efficiendi. Vis luptatum voluptaria in.'
+    },
+    {
+        img:'img/forest.jpg',
+        title: 'Dark forest',
+        description: 'Lorem ipsum dolor sit amet, libris iisque scripta et mel. Autem definiebas philosophia ea vis. Eos sanctus omittam molestiae at, eius viris audiam at per. Tale labitur virtute nam in, ei cum hinc ridens efficiendi. Vis luptatum voluptaria in.'
+    },
+    {
+        img:'img/aurora.jpg',
+        title: 'Aurora over the tree',
+        description: 'Lorem ipsum dolor sit amet, libris iisque scripta et mel. Autem definiebas philosophia ea vis. Eos sanctus omittam molestiae at, eius viris audiam at per. Tale labitur virtute nam in, ei cum hinc ridens efficiendi. Vis luptatum voluptaria in.'
+    },
+    {
+        img:'img/house.jpg',
+        title: 'Creepy house',
+        description: 'Lorem ipsum dolor sit amet, libris iisque scripta et mel. Autem definiebas philosophia ea vis. Eos sanctus omittam molestiae at, eius viris audiam at per. Tale labitur virtute nam in, ei cum hinc ridens efficiendi. Vis luptatum voluptaria in.'
+    },
+    {
+        img:'img/ships.jpg',
+        title: 'The skeletons ships',
+        description: 'Lorem ipsum dolor sit amet, libris iisque scripta et mel. Autem definiebas philosophia ea vis. Eos sanctus omittam molestiae at, eius viris audiam at per. Tale labitur virtute nam in, ei cum hinc ridens efficiendi. Vis luptatum voluptaria in.'
+    },
+    {
+        img:'img/otherPlanet.jpg',
+        title: 'Mountains of another planet',
+        description: 'Lorem ipsum dolor sit amet, libris iisque scripta et mel. Autem definiebas philosophia ea vis. Eos sanctus omittam molestiae at, eius viris audiam at per. Tale labitur virtute nam in, ei cum hinc ridens efficiendi. Vis luptatum voluptaria in.'
+    },
+    {
+        img:'img/blackHole.jpg',
+        title: 'The Black hole event horizon',
+        description: 'Lorem ipsum dolor sit amet, libris iisque scripta et mel. Autem definiebas philosophia ea vis. Eos sanctus omittam molestiae at, eius viris audiam at per. Tale labitur virtute nam in, ei cum hinc ridens efficiendi. Vis luptatum voluptaria in.'
+    }
+
+];
