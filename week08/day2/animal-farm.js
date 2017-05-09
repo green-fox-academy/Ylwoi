50ced4c5ec7e57df5971269249fb2cde69a15486
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-09 17:27:54 +0200

    least hungry animal remover method Done!

--- week08/day2/animal-farm.js
+++ week08/day2/animal-farm.js
@@ -24,11 +24,30 @@ function Farm(slots) {
         if (this.animals.length < this.freePlace) {
             var a = new Animal(name);
             this.animals.push(a);
+        } else {
+            console.log('The farm is full.');
         }
+    };
+    this.slaughter = function () {
+        var least = this.animals[0];
+        this.animals.forEach(function (e) {
+            if (e.hunger < least.hunger) {
+                least = e;
+            }
+        });
+        this.animals.splice(this.animals.indexOf(least), 1);
     }
 }
 
-var farm1 = new Farm(2);
+var farm1 = new Farm(3);
 farm1.breed('tiger');
 farm1.breed('lion');
+farm1.breed('dragon');
+console.log(farm1.animals);
+
+farm1.animals[0].play();
+farm1.animals[1].eat();
+console.log(farm1.animals);
+
+farm1.slaughter();
 console.log(farm1.animals);
