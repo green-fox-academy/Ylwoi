8f9f12c38880388633633d51792e711f4e93cf8f
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-10 13:51:41 +0200

    event listener added to button to change visibility in timeout

--- week08/day3/delayed-click.js
+++ week08/day3/delayed-click.js
@@ -1,3 +1,11 @@
 /**
  * Created by cw on 2017-05-10.
  */
+var button = document.querySelector('button');
+var text = document.querySelector('p');
+
+button.addEventListener('click', function () {
+   setTimeout(function () {
+       text.style.visibility = 'visible';
+   }, 2000)
+});
