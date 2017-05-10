08a54b85b28abbd5e30d72ace135636b1f972af2
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-10 15:27:56 +0200

    increases candies status by one when clicking on create candies button

--- week08/day3/candy-shop.js
+++ week08/day3/candy-shop.js
@@ -6,7 +6,14 @@
 var createCandy = document.querySelector('.create-candies');
 var buyLollipop = document.querySelector('.buy-lollypops');
 var candyMachine = document.querySelector('.candy-machine');
+
 var candyStatus = document.querySelector('.candies');
 var lollyPopStatus = document.querySelector('.lollypops');
 var speedStatus = document.querySelector('.speed');
 
+var candyCounter = candyStatus.textContent;
+
+createCandy.addEventListener('click', function () {
+    candyCounter++;
+    candyStatus.textContent = candyCounter;
+});
