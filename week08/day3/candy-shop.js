b7192c03fe81a01e18d2b623f61f991431dfc4fb
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-10 15:15:48 +0200

    element variables created

--- week08/day3/candy-shop.js
+++ week08/day3/candy-shop.js
@@ -1,3 +1,12 @@
 /**
  * Created by cw on 2017-05-10.
  */
+'use strict';
+
+var createCandy = document.querySelector('.create-candies');
+var buyLollipop = document.querySelector('.buy-lollypops');
+var candyMachine = document.querySelector('.candy-machine');
+var candyStatus = document.querySelector('.candies');
+var lollyPopStatus = document.querySelector('.lollypops');
+var speedStatus = document.querySelector('.speed');
+
