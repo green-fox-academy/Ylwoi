301dc1f1334ad6595f6da308392d94849c5bcf0d
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-10 16:01:27 +0200

    event added to buy lollipop button and set interval to change candy speed

--- week08/day3/candy-shop.js
+++ week08/day3/candy-shop.js
@@ -12,8 +12,25 @@ var lollyPopStatus = document.querySelector('.lollypops');
 var speedStatus = document.querySelector('.speed');
 
 var candyCounter = candyStatus.textContent;
+var lollyPopCounter = lollyPopStatus.textContent.length;
+var speedCounter = speedStatus.textContent;
 
 createCandy.addEventListener('click', function () {
     candyCounter++;
     candyStatus.textContent = candyCounter;
 });
+
+buyLollipop.addEventListener('click', function () {
+    if (candyCounter >= 100) {
+        candyCounter -= 100;
+        candyStatus.textContent = candyCounter;
+        lollyPopStatus.textContent +='🍭';
+    }
+});
+
+setInterval(function () {
+    if (lollyPopCounter % 10 === 0) {
+        speedCounter = lollyPopCounter/10;
+        speedStatus.textContent = speedCounter;
+    }
+}, 1);
