d98c02506f878a7c4558c55e6d924604ffee60cb
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-10 11:56:18 +0200

    logs apple after 3 seconds

---
+++ week08/day3/simple-timeout.js
@@ -0,0 +1,12 @@
+/**
+ * Created by cw on 2017-05-10.
+ */
+'use strict';
+
+// Write a program that prints apple after 3 seconds.
+
+function printApple() {
+    console.log('Apple');
+}
+
+setTimeout(printApple, 3000);
