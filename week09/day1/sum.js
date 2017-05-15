cfa5f166a12a49523dec8e53c35f0b5203127d4d
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-15 12:37:56 +0200

    a method created in  an object which sums an integer list elements

---
+++ week09/day1/sum.js
@@ -0,0 +1,18 @@
+/**
+ * Created by cw on 2017-05-15.
+ */
+'use strict';
+
+var object = {
+    sum: function (list) {
+        let sum = 0;
+        list.forEach(function (e) {
+            sum += e;
+        });
+        return sum;
+    }
+};
+
+console.log(object.sum([1,2,3]));
+
+module.exports = object.sum;
