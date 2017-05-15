9e13b1c8f26bfb9df10a4cb9381cfd1a1493dd8d
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-15 08:57:25 +0200

    city searcher worksgit add .!

--- week08/day4/get-position.js
+++ week08/day4/get-position.js
@@ -1,3 +1,41 @@
 /**
  * Created by cw on 2017-05-11.
  */
+'use strict';
+//lat: parseInt(arr.Results[0].lat), lng: parseInt(arr.Results[0].lon)
+
+
+var httpRequest = new XMLHttpRequest();
+var button = document.querySelector('button');
+var input =  document.querySelector('input');
+var lat = -34;
+var lon = 150;
+
+function engine() {
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+            var arr = JSON.parse(httpRequest.response);
+            lat = parseInt(arr.Results[0].lat);
+            lon = parseInt(arr.Results[0].lon);
+            console.log(parseInt(arr.Results[0].lat));
+            initMap();
+
+        }
+    };
+    httpRequest.open('GET', "https://devru-latitude-longitude-find-v1.p.mashape.com/latlon.php?location=" + input.value );
+    httpRequest.setRequestHeader("X-Mashape-Key", "mHgKaw43rSmshT0JkbzcnwXH0vc4p15mIGGjsnIS8YAkj3WB9b");
+    httpRequest.send();
+}
+
+function initMap() {
+    console.log(lat);
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: lat, lng: lon},
+        scrollwheel: false,
+        zoom: 8
+    })
+}
+
+button.addEventListener('click', function () {
+    engine();
+});
