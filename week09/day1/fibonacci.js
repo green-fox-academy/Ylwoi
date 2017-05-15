0ad1c5538f2e05700ed378d791e016d1ed6cf5b5
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-15 15:43:42 +0200

    function: ceils the index number

--- week09/day1/fibonacci.js
+++ week09/day1/fibonacci.js
@@ -3,9 +3,14 @@
  */
 function fibonacci(index) {
     if (typeof index === "number") {
+        index = Math.ceil(index);
         return (Math.pow((1 + Math.sqrt(5)), index) - Math.pow((1 - Math.sqrt(5)), index)) / (Math.sqrt(5) * Math.pow(2, index));
+    } else {
+        return 'It is not an integer'
     }
 
 }
+
+console.log(fibonacci('1'));
 
 module.exports = fibonacci;
