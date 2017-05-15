222edacaa51e9f131012b1ae5f446f672647ab43
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-15 15:31:20 +0200

    function: if statement added for number type parameters

--- week09/day1/fibonacci.js
+++ week09/day1/fibonacci.js
@@ -2,7 +2,10 @@
  * Created by cw on 2017-05-15.
  */
 function fibonacci(index) {
-    return (Math.pow((1 + Math.sqrt(5)), index) - Math.pow((1 - Math.sqrt(5)), index)) / (Math.sqrt(5) * Math.pow(2, index));
+    if (typeof index === "number") {
+        return (Math.pow((1 + Math.sqrt(5)), index) - Math.pow((1 - Math.sqrt(5)), index)) / (Math.sqrt(5) * Math.pow(2, index));
+    }
+
 }
 
 module.exports = fibonacci;
