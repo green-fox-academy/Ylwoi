0e7b2b01e8ee152347ab69dbd11ecebe14d51ea1
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-16 11:46:13 +0200

    exception handling added

--- week08/day2/sharpie.js
+++ week08/day2/sharpie.js
@@ -4,6 +4,11 @@
 'use strict';
 
 function sharpie (color, width) {
+    if (typeof color !== 'string') {
+        throw new Error('Invalid color parameter')
+    } else if (typeof width !== 'number') {
+        throw new Error('Invalid width parameter')
+    }
     this.color = color;
     this.width = width;
     this.inkAmount = 100;
