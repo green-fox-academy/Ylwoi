033cac626c925cc989735dd76804722d169a8236
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-19 09:49:21 +0200

    database connection created

--- week09/day5/backend.js
+++ week09/day5/backend.js
@@ -1,3 +1,29 @@
 /**
  * Created by cw on 2017-05-19.
  */
+'use strict';
+
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const app = express;
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'posts'
+});
+
+conn.connect(function (err) {
+    if (err) {
+        console.log('Error connecting to DB');
+        return
+    } else {
+        console.log('Connection Established')
+    }
+});
+
+
+app.listen(3000);
