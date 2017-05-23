5219f4bbeafe1388e2257674e61585de0a3c8682
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-23 13:18:05 +0200

    exercise done with factory pattern

--- week10/day2/01.js
+++ week10/day2/01.js
@@ -7,6 +7,9 @@
 // it should take one parameter: what the animal says
 // Every animal should have a method called say() that prints what the animal says
 
+
+//prototype
+
 function Animals (animalSays) {
     this.animalSays = animalSays;
 }
@@ -20,3 +23,21 @@ var cat = new Animals('Meow');
 
 dog.say();
 cat.say();
+
+
+//factory pattern
+
+function FactoryAnimals(animalSays) {
+
+    function say() {
+        return animalSays
+    }
+
+    return {
+        say
+    }
+}
+
+var lion = FactoryAnimals('Roar');
+
+console.log(lion.say());
