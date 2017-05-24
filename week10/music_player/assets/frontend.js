f166d3223db5dc9097fc0930ddb70f1113a2d5b9
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-24 14:57:14 +0200

    displaying playlists from server request

--- week10/music_player/assets/frontend.js
+++ week10/music_player/assets/frontend.js
@@ -3,29 +3,53 @@
  */
 'use strict';
 
-var xhr = new XMLHttpRequest();
+let xhr = new XMLHttpRequest();
 
-let getPlaylists = function () {
+let getPlaylists = function (callback) {
     xhr.open('GET', 'http://localhost:3000/playlists', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send();
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
-            console.log(" PLAYLIST GUD");
-            getTracks()
+            callback(xhr.response)
         }
     }
 };
 
-let getTracks = function () {
+let getTracks = function (callback) {
     xhr.open('GET', 'http://localhost:3000/playlist-tracks', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send();
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
-            console.log('TRACKS GUD');
+            callback(xhr.response)
         }
     }
 };
 
-getPlaylists();
+let displayPlaylists = function (res) {
+    let respPlaylist = JSON.parse(res);
+    console.log(respPlaylist);
+
+    let lengthPlaylists = respPlaylist.length;
+    for (let i = 0; i < lengthPlaylists; i++) {
+        var htmlPLaylists = document.querySelector('.playlists');
+
+        let divPlaylist = document.createElement('div');
+        divPlaylist.setAttribute('class', 'playlist new');
+
+        let h2Playlist = document.createElement('h2');
+        h2Playlist.setAttribute('class', 'list-name');
+        h2Playlist.innerText = respPlaylist[i].title;
+        divPlaylist.appendChild(h2Playlist);
+
+        let anchPlaylist = document.createElement('a');
+        anchPlaylist.setAttribute('class', 'delete-list');
+        if (respPlaylist[i].system === 0) {
+            anchPlaylist.innerText = 'X';
+        }
+        divPlaylist.appendChild(anchPlaylist);
+
+        htmlPLaylists.appendChild(divPlaylist);
+    }
+};
+
+getPlaylists(displayPlaylists);
