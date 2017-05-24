ee44520c8cda5f73cb5a4c6c995cad5ffd916376
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-24 13:43:28 +0200

    xml requests created

--- week10/music_player/assets/frontend.js
+++ week10/music_player/assets/frontend.js
@@ -4,3 +4,28 @@
 'use strict';
 
 var xhr = new XMLHttpRequest();
+
+let getPlaylists = function () {
+    xhr.open('GET', 'http://localhost:3000/playlists', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            console.log(" PLAYLIST GUD");
+            getTracks()
+        }
+    }
+};
+
+let getTracks = function () {
+    xhr.open('GET', 'http://localhost:3000/playlist-tracks', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            console.log('TRACKS GUD');
+        }
+    }
+};
+
+getPlaylists();
