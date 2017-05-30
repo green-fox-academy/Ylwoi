e4a1a551739ea2adc6d4e1a9aef0f328b0c51db9
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-30 15:41:11 +0200

    feat(todos: backend): express server created, endpoint created to send index.html, middleware function called to serve static files

--- week10/todos/server.js
+++ week10/todos/server.js
@@ -1,3 +1,18 @@
 /**
  * Created by cw on 2017-05-30.
  */
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.use('/assets', express.static('assets'));
+app.use('/img', express.static('img'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
+});
+
+
+app.listen(3000);
