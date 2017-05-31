97d7ae972793731be8d95c85bcd5aa47b177f862
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-31 15:44:25 +0200

    feat(todos: frontend): class created for ajax requests, todo get request function created inside in it

--- week10/todos/assets/script.js
+++ week10/todos/assets/script.js
@@ -2,3 +2,20 @@
  * Created by cw on 2017-05-30.
  */
 'use strict';
+
+class Ajax {
+    constructor() {
+        this.xhr = new XMLHttpRequest();
+    }
+
+    getTodo (callback) {
+        this.xhr.open('GET', 'http://localhost:3000/todos', true);
+        this.xhr.send();
+        this.xhr.onreadystatechange = function () {
+            if (this.xhr.readyState === 4) {
+                callback(this.xhr.response)
+            }
+        }
+        }
+
+}
