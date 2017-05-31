b0fe00d8d281f3d5e6ce59405075e09ea9fea570
Horvath Balazs <balazs.horvath989@gmail.com>
2017-05-31 17:57:37 +0200

    feat(todos: frontend): class created with a function to display all todos from DB

--- week10/todos/assets/script.js
+++ week10/todos/assets/script.js
@@ -19,3 +19,49 @@ class Ajax {
         }
 
 }
+
+class Display {
+    displayAllTodo (res) {
+        this.respAllTodo = JSON.parse(res);
+
+        this.lengthTodos = this.respAllTodo.length;
+        this.htmlTodos = document.querySelector('.todos');
+        this.htmlTodos.innerHTML = '';
+
+        for (let i = 0; i < this.lengthTodos; i++) {
+
+            this.divTodo = document.createElement('div');
+            this.divTodo.setAttribute('class', 'Todo');
+
+            this.aTodoName = document.createElement('a');
+            this.aTodoName.setAttribute('class', 'todo-name');
+            this.aTodoName.innerText = this.respAllTodo[i].text;
+            this.divTodo.appendChild(this.aTodoName);
+
+            this.divButtons = document.createElement('div');
+            this.divButtons.setAttribute('class', 'todo-buttons');
+
+            this.imgDelete = document.createElement('img');
+            this.imgDelete.setAttribute('class', 'delete');
+            this.imgDelete.setAttribute('src', 'img/delete.svg');
+            this.divButtons.appendChild(this.imgDelete);
+
+            this.imgCheck = document.createElement('img');
+            this.imgCheck.setAttribute('class', 'check');
+            if (this.respAllTodo[i].completed) {
+                this.imgCheck.setAttribute('src', 'img/checked.svg')
+            } else {
+                this.imgCheck.setAttribute('src', 'img/unchecked.svg')
+            }
+            this.divButtons.appendChild(this.imgCheck);
+
+            this.divTodo.appendChild(this.divButtons);
+            this.htmlTodos.appendChild(this.divTodo);
+        }
+    }
+}
+
+const ajax = new Ajax();
+const disp = new Display();
+
+ajax.getTodo(disp.displayAllTodo);
