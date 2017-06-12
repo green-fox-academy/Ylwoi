4e0a26c21e300f5880a1a8d91396e7b8ad1b2956
Horvath Balazs <balazs.horvath989@gmail.com>
2017-06-12 09:49:29 +0200

    b1

--- week12/day1/pokerTest.js
+++ week12/day1/pokerTest.js
@@ -1,3 +1,11 @@
 /**
  * Created by cw on 2017-06-12.
  */
+'use strict';
+
+var test = require('tape');
+
+test('No same cards', function (t) {
+    t.equal(validator('Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH', true));
+    t.end();
+});
