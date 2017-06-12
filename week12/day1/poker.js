b4178e88643dddcf479b25c0e3ee30f0b7f072c9
Horvath Balazs <balazs.horvath989@gmail.com>
2017-06-12 10:33:59 +0200

    b2

--- week12/day1/poker.js
+++ week12/day1/poker.js
@@ -5,22 +5,19 @@
 
 const validator = function(inputString) {
     let inputArray = inputString.split(' ');
-    let condition = false;
+    let condition = true;
     for (let i = 0; i < inputArray.length; i++) {
         for (let j = 0; j < inputArray.length; j++) {
             if (i != j && inputArray[i] === inputArray[j]) {
-                condition = false;
+                condition *= false;
             } else {
-                condition = true;
+                condition *= true;
             }
         }
     }
-    console.log(condition);
     return condition;
 //    let blackHand = inputString.split(' ', 1,5);
 //    let whiteHand = inputString.split(' ', 7,11);
 };
-
-validator('Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH')
 
 module.exports = validator;
--- week12/day1/pokerTest.js
+++ week12/day1/pokerTest.js
@@ -4,9 +4,10 @@
 'use strict';
 
 var test = require('tape');
-var validator = require('./poker.js')
+var validator = require('./poker.js');
 
 test('No same cards', function (t) {
-    t.equal(validator('Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH'), true);
+    t.equal(validator('Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH'), 1);
+    t.equal(validator('Black: 2H 3D 5S 9C KD  White: 9C 3H 4S 8C AH'), 0);
     t.end();
 });
