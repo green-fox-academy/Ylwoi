102a87548496dbf23940ad997531dfdc39e59f71
Horvath Balazs <balazs.horvath989@gmail.com>
2017-06-12 11:46:25 +0200

    b3g3

--- week12/day1/poker.js
+++ week12/day1/poker.js
@@ -15,9 +15,49 @@ const validator = function(inputString) {
             }
         }
     }
+    console.log(condition);
     return condition;
-//    let blackHand = inputString.split(' ', 1,5);
-//    let whiteHand = inputString.split(' ', 7,11);
+//
 };
 
-module.exports = validator;
+const comparer = function(inputString) {
+    if (validator(inputString)) {
+        const cardRanking = [2, 3, 4, 5, 6, 7, 8, 9, 'T', 'J', 'Q', 'K', 'A'];
+        let blackHand = inputString.split(' ').slice(1,6);
+        let whiteHand = inputString.split(' ').splice(8,12);
+        let hands = {
+            player1: {
+                cards: [
+                    {value: '', color: ''},
+                    {value: '', color: ''},
+                    {value: '', color: ''},
+                    {value: '', color: ''},
+                    {value: '', color: ''}
+                ]
+            },
+            player2: {
+                cards: [
+                    {value: '', color: ''},
+                    {value: '', color: ''},
+                    {value: '', color: ''},
+                    {value: '', color: ''},
+                    {value: '', color: ''}
+                ]
+            }
+        };
+        for (let i = 0; i < blackHand.length; i++) {
+            hands.player1.cards[i].value = blackHand[i].charAt(0);
+            hands.player1.cards[i].color = blackHand[i].charAt(1);
+
+            hands.player2.cards[i].value = whiteHand[i].charAt(0);
+            hands.player2.cards[i].color = whiteHand[i].charAt(1)
+        }
+    }
+};
+
+comparer('Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH');
+
+module.exports = {
+    validator,
+    comparer
+};
--- week12/day1/pokerTest.js
+++ week12/day1/pokerTest.js
@@ -4,10 +4,15 @@
 'use strict';
 
 var test = require('tape');
-var validator = require('./poker.js');
+var poker = require('./poker.js');
 
 test('No same cards', function (t) {
-    t.equal(validator('Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH'), 1);
-    t.equal(validator('Black: 2H 3D 5S 9C KD  White: 9C 3H 4S 8C AH'), 0);
+    t.equal(poker.validator('Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH'), 1);
+    t.equal(poker.validator('Black: 2H 3D 5S 9C KD  White: 9C 3H 4S 8C AH'), 0);
+    t.end();
+});
+
+test('High card wins', function (t) {
+    t.equal(poker.comparer('Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH'), 'White wins. - with high card: Ace ');
     t.end();
 });
